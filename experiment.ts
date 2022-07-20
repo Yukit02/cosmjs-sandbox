bac3294777fe26605eda60681aa1a16bb580f436
Yuki <59007319+Yukit02@users.noreply.github.com>
2022-07-20 12:51:17 +0900

    feat: fetch tx info by id

--- experiment.ts
+++ experiment.ts
@@ -1,6 +1,7 @@
-import { StargateClient } from "@cosmjs/stargate"
+import { StargateClient, IndexedTx } from "@cosmjs/stargate"
 const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz"
 const aliceAddress = "cosmos17tvd4hcszq7lcxuwzrqkepuau9fye3dal606zf"
+const txId = "540484BDD342702F196F84C2FD42D63FA77F74B26A8D7383FAA5AB46E4114A9B"
 
 const runAll = async (): Promise<void> => {
   const client = await StargateClient.connect(rpc);
@@ -9,6 +10,8 @@ const runAll = async (): Promise<void> => {
     "Alice balances:",
     await client.getAllBalances(aliceAddress),
   );
+  const faucetTx: IndexedTx = (await client.getTx(txId))!;
+  console.log("Faucet Tx:", faucetTx)
 }
 
 runAll();
