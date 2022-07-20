87a6adc76e76d55ffc335710afb8d8b5523efaec
Yuki <59007319+Yukit02@users.noreply.github.com>
2022-07-20 11:21:49 +0900

    feat: get balances

--- experiment.ts
+++ experiment.ts
@@ -1,5 +1,14 @@
+import { StargateClient } from "@cosmjs/stargate"
+const rpc = "https://rpc.sentry-01.theta-testnet.polypore.xyz"
+const aliceAddress = "cosmos17tvd4hcszq7lcxuwzrqkepuau9fye3dal606zf"
+
 const runAll = async (): Promise<void> => {
-  console.log("TODO");
+  const client = await StargateClient.connect(rpc);
+  console.log("With client, chain id:", await client.getChainId(), ", height:", await client.getHeight());
+  console.log(
+    "Alice balances:",
+    await client.getAllBalances(aliceAddress),
+  );
 }
 
 runAll();
